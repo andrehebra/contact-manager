ba0a4bea32db4d7933ec00ad92f2bd9733bdc9a9
HashDBrown <halkhateeb704@gmail.com>
2024-05-30 01:08:13 -0400

    Edit contacts is now working!

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ const extension = 'php';
 let userId = 0;
 let firstName = "";
 let lastName = "";
+const id_array = [];
 
 
 function validateUsername() {
@@ -62,8 +63,6 @@ function doSignup() {
     if (usernameMessage || passwordMessage || errorMessage) {
         return false;
     }
-
-    // Proceed with the signup process if validation passes
 
     var hash = md5(password);
 
@@ -200,32 +199,6 @@ function doLogout() {
     window.location.href = "main.html";
 }
 
-function addColor() {
-    let newColor = document.getElementById("colorText").value;
-    document.getElementById("colorAddResult").innerHTML = "";
-
-    let tmp = { color: newColor, userId, userId };
-    let jsonPayload = JSON.stringify(tmp);
-
-    let url = urlBase + '/AddColor.' + extension;
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    try {
-        xhr.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("colorAddResult").innerHTML = "Color has been added";
-            }
-        };
-        xhr.send(jsonPayload);
-    }
-    catch (err) {
-        document.getElementById("colorAddResult").innerHTML = err.message;
-    }
-
-}
-
 
 function addContact() {
     let firstName = document.getElementById("addFirstName").value;
@@ -296,45 +269,6 @@ function addContact() {
 }
 
 
-
-function searchColor() {
-    let srch = document.getElementById("searchText").value;
-    document.getElementById("colorSearchResult").innerHTML = "";
-
-    let colorList = "";
-
-    let tmp = { search: srch, userId: userId };
-    let jsonPayload = JSON.stringify(tmp);
-
-    let url = urlBase + '/SearchColors.' + extension;
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    try {
-        xhr.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
-                let jsonObject = JSON.parse(xhr.responseText);
-
-                for (let i = 0; i < jsonObject.results.length; i++) {
-                    colorList += jsonObject.results[i];
-                    if (i < jsonObject.results.length - 1) {
-                        colorList += "<br />\r\n";
-                    }
-                }
-
-                document.getElementsByTagName("p")[0].innerHTML = colorList;
-            }
-        };
-        xhr.send(jsonPayload);
-    }
-    catch (err) {
-        document.getElementById("colorSearchResult").innerHTML = err.message;
-    }
-}
-
-
 function searchContacts(event) {
     event.preventDefault(); // Prevent form submission and page reload
     console.log("searching contacts");
@@ -367,16 +301,16 @@ function searchContacts(event) {
                 }
                 let text = "<table border='1'>"
                 for (let i = 0; i < jsonObject.results.length; i++) {
-                    //ids[i] = jsonObject.results[i].ID
+                    id_array[i] = jsonObject.results[i].ID
                     text += "<tr id='row" + i + "'>"
                     text += "<td id='first_Name" + i + "'><span>" + jsonObject.results[i].FirstName + "</span></td>";
                     text += "<td id='last_Name" + i + "'><span>" + jsonObject.results[i].LastName + "</span></td>";
                     text += "<td id='email" + i + "'><span>" + jsonObject.results[i].Email + "</span></td>";
                     text += "<td id='phone" + i + "'><span>" + jsonObject.results[i].Phone + "</span></td>";
                     text += "<td>" +
-                        "<button type='button' id='edit_button" + i + "' class='w3-button w3-circle w3-lime' onclick='edit_row(" + i + ")'>" + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/></svg>' + "</button>" +
-                        "<button type='button' id='save_button" + i + "' value='Save' class='w3-button w3-circle w3-lime' onclick='save_row(" + i + ")' style='display: none'>" + "<span class='glyphicon glyphicon-saved'></span>" + "</button>" +
-                        "<button type='button' onclick='delete_row(" + i + ")' class='w3-button w3-circle w3-amber'>" + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16"><path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/></svg>' + "</button>" + "</td>";
+                        "<button type='button' id='edit_button" + i + "' class='w3-button w3-circle w3-lime' onclick='edit_row(" + i + ")' aria-label='Edit Row " + i + "'>" + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/></svg>' + "</button>" +
+                        "<button type='button' id='save_button" + i + "' value='Save' class='w3-button w3-circle w3-lime' onclick='save_row(" + i + ")' style='display: none' aria-label='Save Row " + i + "'>" + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/></svg>' + "<span class='glyphicon glyphicon-saved'></span>" + "</button>" +
+                        "<button type='button' onclick='delete_row(" + i + ")' class='w3-button w3-circle w3-amber' aria-label='Delete Row " + i + "'>" + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16"><path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/></svg>' + "</button>" + "</td>";
                     text += "<tr/>"
                 }
                 text += "</table>"
@@ -421,7 +355,7 @@ function loadContacts() {
                 }
                 let text = "<table border='1'>"
                 for (let i = 0; i < jsonObject.results.length; i++) {
-                    //ids[i] = jsonObject.results[i].ID
+                    id_array[i] = jsonObject.results[i].ID
                     text += "<tr id='row" + i + "'>"
                     text += "<td id='first_Name" + i + "'><span>" + jsonObject.results[i].FirstName + "</span></td>";
                     text += "<td id='last_Name" + i + "'><span>" + jsonObject.results[i].LastName + "</span></td>";
@@ -429,7 +363,7 @@ function loadContacts() {
                     text += "<td id='phone" + i + "'><span>" + jsonObject.results[i].Phone + "</span></td>";
                     text += "<td>" +
                         "<button type='button' id='edit_button" + i + "' class='w3-button w3-circle w3-lime' onclick='edit_row(" + i + ")' aria-label='Edit Row " + i + "'>" + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/></svg>' + "</button>" +
-                        "<button type='button' id='save_button" + i + "' value='Save' class='w3-button w3-circle w3-lime' onclick='save_row(" + i + ")' style='display: none' aria-label='Save Row " + i + "'>" + "<span class='glyphicon glyphicon-saved'></span>" + "</button>" +
+                        "<button type='button' id='save_button" + i + "' value='Save' class='w3-button w3-circle w3-lime' onclick='save_row(" + i + ")' style='display: none' aria-label='Save Row " + i + "'>" + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/></svg>' + "<span class='glyphicon glyphicon-saved'></span>" + "</button>" +
                         "<button type='button' onclick='delete_row(" + i + ")' class='w3-button w3-circle w3-amber' aria-label='Delete Row " + i + "'>" + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16"><path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/></svg>' + "</button>" + "</td>";
                     text += "<tr/>"
                 }
@@ -446,22 +380,108 @@ function loadContacts() {
 }
 
 function delete_row(i) {
-    let rowId = "row" + i;
+    if (confirm("Are you sure you want to delete this contact?")) {
+        let rowId = "row" + i;
+        let firstName = document.getElementById("first_Name" + i).textContent;
+        let lastName = document.getElementById("last_Name" + i).textContent;
+        let email = document.getElementById("email" + i).textContent;
+        let phone = document.getElementById("phone" + i).textContent;
+
+        let tmp = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            phone: phone,
+            userId: userId
+        };
+
+        let jsonPayload = JSON.stringify(tmp);
+        let url = urlBase + '/DeleteContact.' + extension;
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+        try {
+            xhr.onreadystatechange = function () {
+                if (this.readyState != 4) {
+                    return;
+                }
+
+                if (this.readyState == 4 && this.status == 200) {
+                    let response = JSON.parse(xhr.responseText);
+                    if (response.error) {
+                        console.log("Error: " + response.error);
+                    } else {
+                        console.log("Contact Deleted Successfully!");
+                        document.getElementById(rowId).remove();
+                    }
+                }
+            };
+            xhr.send(jsonPayload);
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+}
+
+function edit_row(i) {
+    document.getElementById("edit_button" + i).style.display = "none";
+    document.getElementById("save_button" + i).style.display = "inline-block";
+
     let firstName = document.getElementById("first_Name" + i).textContent;
     let lastName = document.getElementById("last_Name" + i).textContent;
     let email = document.getElementById("email" + i).textContent;
     let phone = document.getElementById("phone" + i).textContent;
 
+    document.getElementById("first_Name" + i).innerHTML = "<input type='text' id='firstName_text" + i + "' value='" + firstName + "'>";
+    document.getElementById("last_Name" + i).innerHTML = "<input type='text' id='lastName_text" + i + "' value='" + lastName + "'>";
+    document.getElementById("email" + i).innerHTML = "<input type='text' id='email_text" + i + "' value='" + email + "'>";
+    document.getElementById("phone" + i).innerHTML = "<input type='text' id='phone_text" + i + "' value='" + phone + "'>";
+}
+
+function save_row(i) {
+    let firstName = document.getElementById("firstName_text" + i).value;
+    let lastName = document.getElementById("lastName_text" + i).value;
+    let email = document.getElementById("email_text" + i).value;
+    let phone = document.getElementById("phone_text" + i).value;
+    let curr_id = id_array[i];
+
+    let phoneRegex = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
+    let emailRegex = /^([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,5})$/;
+
+    if (!firstName) {
+        result.innerHTML += 'First name cannot be blank. ';
+    }
+    if (!lastName) {
+        result.innerHTML += 'Last name cannot be blank. ';
+    }
+    if (!phoneRegex.test(phone)) {
+        result.innerHTML += "Phone Number must be in the format ###-###-####. ";
+    }
+    if (!emailRegex.test(email)) {
+        result.innerHTML += "Email must be in the correct format. ";
+    }
+
+    document.getElementById("first_Name" + i).innerHTML = firstName;
+    document.getElementById("last_Name" + i).innerHTML = lastName;
+    document.getElementById("email" + i).innerHTML = email;
+    document.getElementById("phone" + i).innerHTML = phone;
+
+    document.getElementById("edit_button" + i).style.display = "inline-block";
+    document.getElementById("save_button" + i).style.display = "none";
+
     let tmp = {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        phone: phone,
-        userId: userId
+        ID: curr_id, // Assuming the row index corresponds to the contact ID
+        FirstName: firstName,
+        LastName: lastName,
+        Phone: phone,
+        Email: email,
+        UserId: userId
     };
 
     let jsonPayload = JSON.stringify(tmp);
-    let url = urlBase + '/DeleteContact.' + extension;
+    let url = urlBase + '/EditContacts.' + extension;
 
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
@@ -478,13 +498,12 @@ function delete_row(i) {
                 if (response.error) {
                     console.log("Error: " + response.error);
                 } else {
-                    console.log("Contact Deleted Successfully!");
-                    document.getElementById(rowId).remove();
+                    console.log("Contact Updated Successfully!");
                 }
             }
         };
         xhr.send(jsonPayload);
-    } catch (err){
+    } catch (err) {
         console.log(err.message);
     }
 }
